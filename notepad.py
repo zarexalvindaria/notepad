72f571c8f4ed6ffa0921728354814b04beb243f7
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2021-07-26 04:33:11 +0800

    added icons in the menu bar

--- notepad.py
+++ notepad.py
@@ -1,6 +1,7 @@
 # Notepad
 # icon http://www.doublejdesign.co.uk
 import tkinter as tk
+from PIL import ImageTk, Image
 
 # Define window
 root = tk.Tk()
@@ -22,6 +23,23 @@ text_frame = tk.Frame(root, bg=text_color)
 menu_frame.pack(padx=5, pady=5)
 text_frame.pack(padx=5, pady=5)
 
+# Layout for menu frame
+# Create the menu: new, open, save, close, font family, font size, font option
+new_image = ImageTk.PhotoImage(Image.open("img/new.png"))
+new_button = tk.Button(menu_frame, image=new_image)
+new_button.grid(row=0, column=0, padx=5, pady=5)
+
+open_image = ImageTk.PhotoImage(Image.open("img/open.png"))
+open_button = tk.Button(menu_frame, image=open_image)
+open_button.grid(row=0, column=1, padx=5, pady=5)
+
+save_image = ImageTk.PhotoImage(Image.open("img/save.png"))
+save_button = tk.Button(menu_frame, image=save_image)
+save_button.grid(row=0, column=2, padx=5, pady=5)
+
+close_image = ImageTk.PhotoImage(Image.open("img/close.png"))
+close_button = tk.Button(menu_frame, image=close_image, command=root.destroy)
+close_button.grid(row=0, column=3, padx=5, pady=5)
 
 # Run the root window's main loop
 root.mainloop()
