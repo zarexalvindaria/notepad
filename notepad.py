817c28b6099ceda23d2f6ae1ad31346fe9e753b0
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2021-07-26 04:09:51 +0800

    set the window layout and color

--- notepad.py
+++ notepad.py
@@ -1,7 +1,19 @@
 # Notepad
 # icon http://www.doublejdesign.co.uk
-
 import tkinter as tk
 
+# Define window
 root = tk.Tk()
-roo
+root.title("Notepad")
+root.iconbitmap("pad.ico")
+root.geometry("600x600")
+root.resizable(0,0)
+
+# Define fonts and colors
+text_color = "#fffacd"
+menu_color = "#dbd9db"
+root_color = "#6c809a"
+root.config(bg=root_color)
+
+# Run the root window's main loop
+root.mainloop()
